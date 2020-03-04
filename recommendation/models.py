f5291252630a17989677c7064ba2a2c77e747711
akshat gautam <gautam.akshat92@gmail.com>
2020-03-05 00:47:08 +0530

    Initialised project day 1

--- recommendation/models.py
+++ recommendation/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User():
+    pass
+
+class Item():
+    pass
+
+class ItemProfiles():
+    pass
+
+class UserProfiles():
+    pass
